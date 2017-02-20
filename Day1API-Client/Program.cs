3c8e65cf59f68b3aeede23a2bbf46dc610491f2e
ksindorf000 <ksindorf000@gmail.com>
2017-02-20 10:30:35 -0500

    Add project files.

---
+++ Day1API-Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1API_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
